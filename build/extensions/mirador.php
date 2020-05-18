db67db59ff5ccc4ac80b740c23a12dc3e387ae60
Joseph Padfield <joseph.padfield@ng-london.org.uk>
2020-05-18 21:36:00 +0100

    Updating from Mirador 2.7.2 to Mirador 3.0.0beta8

--- build/extensions/mirador.php
+++ build/extensions/mirador.php
@@ -1,5 +1,7 @@
 <?php
 
+// Updated to Mirador V3 18/05/2020
+
 $extensionList["mirador"] = "extensionMirador";
 
 function extensionMirador ($d, $pd)
@@ -16,6 +18,7 @@ function extensionMirador ($d, $pd)
 			{
 			$dets = getRemoteJsonDetails($d["file"], false, false);
 			$dets = explode(PHP_EOL, trim($dets));
+
 			$codeHTML = displayCode ($dets, "The Mirador TXT File", "txt");
 
 			if (preg_match('/^http.+/', $dets[0]))
@@ -40,6 +43,7 @@ function extensionMirador ($d, $pd)
       }
     }
 
+	$pd["extra_css"] .= ".fixed-top {z-index:1111;}";
 	$mirador_path = "https://unpkg.com/mirador@3.0.0-beta.8/dist/";
 	$pd["extra_js_scripts"][] = $mirador_path."mirador.min.js";
 
@@ -56,55 +60,21 @@ var myMiradorInstance = Mirador.viewer({
 END;
 	$pd["extra_js"] .= ob_get_contents();
 	ob_end_clean(); // Don't send output to client
-     
-	/*
-	// https://unpkg.com version 2.7.2 did not seem to work, will try again once V3 is
-	// fully released. - jpadfield 30/03/20
-	$mirador_path = "https://unpkg.com/mirador@2.6.0/dist/";
-	//$mirador_path = "https://tanc-ahrc.github.io/mirador/mirador/";
-	
-  $pd["extra_css_scripts"][] = $mirador_path."css/mirador-combined.min.css";
-	$pd["extra_js_scripts"][] = $mirador_path."mirador.min.js";
-	$pd["extra_js_scripts"][] = $mirador_path."mirador.min.js.map";
-	$pd["extra_js"] .= '
-	$(function() {
-     myMiradorInstance = Mirador({
-       id: "viewer",
-       layout: '.$lo.',
-       buildPath: "'.$mirador_path.'",
-       data: '.$mans.',
-       "windowObjects": '.$wo.'
-       });
-     });';
-  //use to hide the label used for the first line which is just in place to provide a margin/padding on the left.
-	$pd["extra_css"] .= "
-#viewer {       
-      display: block;
-      width: 100%;
-      height: 600px;
-      position: relative;
-     }";*/
-//$d["content"] = positionExtraContent ($d["content"], '<div class="row" style="padding-left:16px;padding-right:16px;"><div class="col-12 col-lg-12"><div style="height:500px;" id="mirador"></div></div></div>');
 
 	$d["content"] = positionExtraContent ($d["content"], '<div class="row" style="padding-left:16px;padding-right:16px;"><div class="col-12 col-lg-12"><div style="height:500px;" id="mirador"></div></div></div>'.$codeHTML);
 
   return (array("d" => $d, "pd" => $pd));
   }
 
-	
+	 
 function listToManifest ($list)
 	{
-	$manifests = array();
+	$manifests = "{";
 
 	foreach ($list as $k => $url)
-		{
-		$manifests[] = array(
-			$url => array("provider" => "Undefined"),
-			);
-		}
-
-	$manifests = json_encode($manifests);
+		{$manifests .= "
+".json_encode($url).":{\"provider\":\"Undefined\"},";}
 	
-	return($manifests);
+	return($manifests."}");
 	}    
 ?>
